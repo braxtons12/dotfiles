68a8da851d9e7d84a9f3d3ccd7166f77edf9fb06
Braxton Salyer <braxtonsalyer@gmail.com>
2025-06-06 21:50:43 -0500

    rework application of nvim-treesitter settings

--- .config/nvim/ftplugin/bash.lua
+++
@@ -1,2 +0,0 @@
-vim.treesitter.start()
-vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
--- .config/nvim/ftplugin/lua.lua
+++
@@ -1,3 +0,0 @@
-vim.treesitter.start()
-vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
-
